d9b5b87b28a4a412b821f793c4a2377fb539ab53
MF-patino <m.fpatino@udc.es>
2025-07-02 16:47:23 +0200

    Initial commit Project has been set up using Spring Initializr

---
+++ src/main/java/com/mf/HerculaneumTranscriptor/HerculaneumTranscriptorApplication.java
@@ -0,0 +1,13 @@
+package com.mf.HerculaneumTranscriptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerculaneumTranscriptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerculaneumTranscriptorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mf/HerculaneumTranscriptor/HerculaneumTranscriptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mf.HerculaneumTranscriptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerculaneumTranscriptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
