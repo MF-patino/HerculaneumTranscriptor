37c89a172a2801e04cf94eeb589d5ac02c55d183
MF-patino <m.fpatino@udc.es>
2025-07-14 20:50:41 +0200

    User JPA entity and mapper implementation
    A CRUD repository interface for User entities was also implemented.

---
+++ src/main/java/com/mf/HerculaneumTranscriptor/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.mf.HerculaneumTranscriptor.repository;
+
+import com.mf.HerculaneumTranscriptor.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+  Boolean existsByUsername(String username);
+  Optional<User> findByUsername(String username);
+}
