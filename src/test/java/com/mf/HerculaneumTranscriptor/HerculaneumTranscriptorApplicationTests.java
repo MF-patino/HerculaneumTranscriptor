e35af0fc6a6c58908b4d8a5a2cfeb716d3f82c91
MF-patino <m.fpatino@udc.es>
2025-07-14 20:47:30 +0200

    Configuration for data persistance
    Regular application deployment will connect to a PostgreSQL Docker instance, while an in-memory H2 database will be used for testing purposes.

--- src/test/java/com/mf/HerculaneumTranscriptor/HerculaneumTranscriptorApplicationTests.java
+++ src/test/java/com/mf/HerculaneumTranscriptor/HerculaneumTranscriptorApplicationTests.java
@@ -2,8 +2,10 @@ package com.mf.HerculaneumTranscriptor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HerculaneumTranscriptorApplicationTests {
 
 	@Test
