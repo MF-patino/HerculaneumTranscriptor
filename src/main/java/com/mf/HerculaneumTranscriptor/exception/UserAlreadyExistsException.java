c730906222d6149cf95a2dc9139cec2e32ecba8e
MF-patino <m.fpatino@udc.es>
2025-07-29 22:05:24 +0200

    Refined exception and CORS handling
    The new GlobalExceptionHandler handles translation of Java exceptions into response entities in a centralized way.
    
    The new CorsConfiguration configuration class enables communication with the React frontend.

--- src/main/java/com/mf/HerculaneumTranscriptor/exception/UserAlreadyExistsException.java
+++ src/main/java/com/mf/HerculaneumTranscriptor/exception/UserAlreadyExistsException.java
@@ -1,15 +1,7 @@
 package com.mf.HerculaneumTranscriptor.exception;
-
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * This exception is thrown when a user tries to register with an already used username or email.
- * The @ResponseStatus annotation tells Spring to automatically translate this exception
- * into an HTTP 33 response, simplifying controller-level error handling.
  */
-@ResponseStatus(value = HttpStatus.CONFLICT)
 public class UserAlreadyExistsException extends RuntimeException {
 
   public UserAlreadyExistsException(String message) {
