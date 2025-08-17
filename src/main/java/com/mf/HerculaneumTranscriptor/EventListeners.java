a3140168c28aebe358e4f69e4a463c88338f0f5a
MF-patino <m.fpatino@udc.es>
2025-08-17 23:07:26 +0200

    Automatic generation of root profile from configuration file
    The method EventListeners.createRootProfileOnStartUp will fire at application startup, creating a new root profile or updating the username and password as needed.
    
    Username and password for the root profile can be changed at any point by editing the application.yml configuration file and restarting the application.

---
+++ src/main/java/com/mf/HerculaneumTranscriptor/EventListeners.java
@@ -0,0 +1,57 @@
+package com.mf.HerculaneumTranscriptor;
+
+import com.mf.HerculaneumTranscriptor.domain.User;
+import com.mf.HerculaneumTranscriptor.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import user.dto.UserInfo;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class EventListeners {
+  @Value( "${security.rootProfile.username}" )
+  private String rootUsername;
+  @Value( "${security.rootProfile.password}" )
+  private String rootPassword;
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  // Event listener to create a root profile on startup if it doesn't exist
+  // In case the root password or username is changed in the configuration file, the root profile will be updated
+  @EventListener(ApplicationReadyEvent.class)
+  public void createRootProfileOnStartUp(ApplicationReadyEvent event) {
+    Optional<User> possibleRoot = userRepository.findByPermissions(UserInfo.PermissionsEnum.ROOT);
+    User root;
+    String rootPasswordHash = passwordEncoder.encode(rootPassword);
+
+    if (possibleRoot.isEmpty()) {
+      System.out.println("Root profile not found, generating one.");
+
+      root = new User();
+      root.setUsername(rootUsername);
+      root.setFirstName("Root");
+      root.setPasswordHash(rootPasswordHash);
+      root.setPermissions(UserInfo.PermissionsEnum.ROOT);
+
+      userRepository.save(root);
+    } else {
+      root = possibleRoot.get();
+
+      if (!passwordEncoder.matches(rootPassword, root.getPasswordHash()) ||
+              !root.getUsername().equals(rootUsername)) {
+        System.out.println("Root profile information not aligned with configuration, updating.");
+
+        root.setPasswordHash(rootPasswordHash);
+        root.setUsername(rootUsername);
+        userRepository.save(root);
+      }
+    }
+  }
+}
--- src/main/java/com/mf/HerculaneumTranscriptor/repository/UserRepository.java
+++ src/main/java/com/mf/HerculaneumTranscriptor/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.mf.HerculaneumTranscriptor.repository;
 
 import com.mf.HerculaneumTranscriptor.domain.User;
 import org.springframework.data.repository.CrudRepository;
+import user.dto.UserInfo;
 
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
   Boolean existsByUsername(String username);
   Optional<User> findByUsername(String username);
+  Optional<User> findByPermissions(UserInfo.PermissionsEnum permissions);
 }
